c5b7d2c83a7dbc91e21b68019854bf66a4de6e8c
Thomas Veens <thomasveens@gmail.com>
2021-06-11 12:59:29 +0200

    implement simple cell binding

---
+++ Sudoku/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sudoku {
+    class Cell : INotifyPropertyChanged {
+        private int cellValue = 0;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int Value {
+            get { return cellValue; }
+            set {
+                cellValue = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
--- Sudoku/MainPage.xaml.cs
+++ Sudoku/MainPage.xaml.cs
@@ -15,16 +15,15 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Sudoku
-{
+namespace Sudoku {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        Cell cell = new Cell { Value = 2 };
+        public MainPage() {
             this.InitializeComponent();
+            this.DataContext = cell;
         }
     }
 }
